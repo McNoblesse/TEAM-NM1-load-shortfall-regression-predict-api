b7a8b5deb2a1bdc7cca251a7461153c77084fad3
McNoblesse <noblesse@gmail.com>
2023-11-17 13:27:30 +0100

    final update

--- model.py
+++ model.py
@@ -73,6 +73,16 @@ def _preprocess_data(data):
        'Barcelona_temp_max', 'Madrid_temp_max', 'Barcelona_temp',
        'Bilbao_temp_min', 'Bilbao_temp', 'Barcelona_temp_min',
        'Bilbao_temp_max', 'Seville_temp_min', 'Madrid_temp', 'Madrid_temp_min']]
+    
+    feature_vector_df['Valencia_pressure'] = feature_vector_df['Valencia_pressure'].fillna(feature_vector_df['Valencia_pressure'].mode()[0])
+    
+    feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].str.extract('(\d+)')
+    feature_vector_df['Valencia_wind_deg'] = pd.to_numeric(feature_vector_df['Valencia_wind_deg'])
+    feature_vector_df['Valencia_wind_deg']
+
+    feature_vector_df['Seville_pressure'] = feature_vector_df['Seville_pressure'].str.extract('(\d+)')
+    feature_vector_df['Seville_pressure'] = pd.to_numeric(feature_vector_df['Seville_pressure'])
+    feature_vector_df['Seville_pressure']
     # ------------------------------------------------------------------------
     return predict_vector
 
